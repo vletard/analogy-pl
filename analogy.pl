e2ca15931275aa141ba418c6ee4963d760e43413
Vincent Letard <letard.vincent@gmail.com>
2021-02-17 20:46:53 -0500

    Added est_size optimization for refined cost estimation.

--- analogy.pl
+++ analogy.pl
@@ -1,12 +1,12 @@
-main :-
+test :-
   string_chars("Une phrase", A),
   string_chars("Une proposition", B),
   string_chars("Des phrases", C),
-  sequence_equation(A, B, C, Solution, [optimize(degree)]),
+  sequence_equation(A, B, C, Solution, [optimize(est_size)]),
   Solution = solution(output(Output), Metric, Factors),
   string_chars(OutputStr, Output),
   format('~p -> ~p~n', [Metric, OutputStr]),
-%  print_factors(Factors),
+  print_factors(Factors),
   true.
 
 get_assoc_default(Key, Assoc, Value, _) :-
@@ -15,65 +15,70 @@ get_assoc_default(Key, Assoc, Value, _) :-
 get_assoc_default(_, _, Default, Default).
 
 sequence_equation(A, B, C, Solution, Options) :-
-  (is_list(Options) ; Options = []),
-  foreach(member(optimize(Metric_), Options), Metric = Metric_),
-  (Metric = degree, !, Init = degree(1) ; Metric = size, Init = size(0)),
+  % sequences setup
   compound_name_arguments(SeqA, seq, A),
   compound_name_arguments(SeqB, seq, B),
   compound_name_arguments(SeqC, seq, C),
   compound_name_arity(SeqA, seq, NA),
   compound_name_arity(SeqB, seq, NB),
   compound_name_arity(SeqC, seq, NC),
+
+  % checking options
+  (is_list(Options) ; Options = []),
+  foreach(member(optimize(Metric_), Options), Metric = Metric_),
+  (
+    Metric == degree, !, Val0 = 1 ;
+    Metric == size, !, Val0 = 0 ;
+    Metric == est_size, max_list([NA, NB, NC], Val0)
+  ),
+  compound_name_arguments(Init, Metric, [Val0]),
+
+  % assoc setup
   empty_assoc(Empty),
   Cursor = coord(0,0,0),
-  put_assoc(Cursor, Empty, solution(
-                             Init,
-                             [factorization([[]],[[]],0,0,_)]
-                           ), Matrix),
-  put_assoc(Init, Empty, [Cursor], Ordered),
-  process_next_partial(((SeqA, SeqB, SeqC), (NA, NB, NC)), Solution, Init, (Empty, Ordered, Matrix)).
+  put_assoc(Cursor, Empty, [factorization([[]],[[]],0,0,_)], SolutionAssoc),
+  put_assoc(Init, Empty, SolutionAssoc, Register),
+  process_next_partial(((SeqA, SeqB, SeqC), (NA, NB, NC)),
+                       Solution,
+                       Init,
+                       (Empty, Register)).
 
-process_next_partial(Sequences, Solution, Current, (Explored, Ordered, Matrix)) :-
-  get_assoc(Current, Ordered, CoordList),
-  member(Coord, CoordList), % TODO avoid unnecessary steps by including a memory
-  get_assoc(Coord, Matrix, solution(Metric, Factorizations)),
-  member(Factorization, Factorizations),
+process_next_partial(Sequences, Solution, Current, (Explored, Register)) :-
+  get_assoc(Current, Register, SolutionAssoc),
+  gen_assoc(Coord, SolutionAssoc, FactorizationList),
+  member(Factorization, FactorizationList),
   \+ get_assoc(Factorization, Explored, _),
   !,
   put_assoc(Factorization, Explored, true, Explored1),
   (Coord = coord(I, J, K), Sequences = (_, (I, J, K)) ->
-    (
-      Solution = solution(output(Output), Metric, FormattedFactorization),
+    ( % the factorization is complete
+      Solution = solution(output(Output), Current, FormattedFactorization),
       format_factorization(Factorization, FormattedFactorization, Output)
       ;
-      process_next_partial(Sequences, Solution, Current, (Explored1, Ordered, Matrix))
+      process_next_partial(Sequences, Solution, Current, (Explored1, Register))
     )
     ;
-    findall((Coord1, Factorization1, Metric1), explore(Sequences, Coord, Factorization, Metric, Coord1, Factorization1, Metric1), Additions),
-    foldl(add_factorization, Additions, (Ordered, Matrix), (Ordered1, Matrix1)),
-    process_next_partial(Sequences, Solution, Current, (Explored1, Ordered1, Matrix1))
+    % not complete
+    findall((Coord1, Factorization1, Metric1), explore(Sequences,
+                                 Coord, Factorization, Current,
+                                 Coord1, Factorization1, Metric1), Additions),
+    foldl(add_factorization, Additions, Register, Register1),
+    process_next_partial(Sequences, Solution, Current, (Explored1, Register1))
   ).
 
-process_next_partial(Sequences, Solution, Current, (Explored, Ordered, Matrix)) :-
-  clear_indices(Current, Explored, Ordered, Matrix, Ordered1, Matrix1),
-  assoc_to_keys(Ordered, [_|_]), % still more than one key on the metric ordered
+process_next_partial(Sequences, Solution, Current, (Explored, Register)) :-
+  del_assoc(Current, Register, _, Register1),
+  gen_assoc(_, Register1, _), % still more than one key on the metric ordered
+  !,
   compound_name_arguments(Current, Metric, [N]),
   N1 is N + 1,
   compound_name_arguments(Next, Metric, [N1]),
-  % Displaying the number of explored partial factorizations:
-%  aggregate_all(count, gen_assoc(_, Explored, _), Size), format('~p ~g~n', [Current, Size]),
+  % displaying the number of explored partial factorizations
+  aggregate_all(count, gen_assoc(_, Explored, _), Size),
+  format('~p: ~g active paths.~n', [Current, Size]),
+  
   empty_assoc(Empty),
-  process_next_partial(Sequences, Solution, Next, (Empty, Ordered1, Matrix1)).
-
-clear_indices(Current, Explored, Ordered, Matrix, Ordered1, Matrix1) :-
-  del_assoc(Current, Ordered, CoordList, Ordered1),
-  clear_indices(Explored, CoordList, Matrix, Matrix1).
-clear_indices(_, [], Matrix, Matrix).
-clear_indices(Explored, [Coord|CoordList], Matrix, Matrix1) :-
-  del_assoc(Coord, Matrix, solution(_, Factorizations), Matrix_),
-  forall((member(Factorization, Factorizations), \+ get_assoc(Factorization, Explored, true)), throw('Inconsistent index state.')),
-  clear_indices(Explored, CoordList, Matrix_, Matrix1).
-
+  process_next_partial(Sequences, Solution, Next, (Empty, Register1)).
 
 explore(((A, B, _), _), coord(I, J, K), Factorization, degree(D), coord(I_, J_, K), Factorization1, degree(D1)) :-
   I_ is I + 1,
@@ -193,33 +198,114 @@ explore(((_, B, _), _), coord(I, J, K), Factorization, size(S), coord(I, J_, K),
   Factorization1 = factorization(LA, [[Item|FD]|LD], SA, SD_, crossed),
   (SA > SD -> S1 is S ; S1 is S + 1).
 
-% Matrix already contains factorizations with the same metric value for Coord
-add_factorization((Coord, Factorization, Metric), (Ordered, Matrix), (Ordered, Matrix1)) :-
-  get_assoc(Coord, Matrix, solution(Metric, Factorizations)),
-  !,
-  put_assoc(Coord, Matrix, solution(Metric, [Factorization|Factorizations]), Matrix1).
-% Matrix contains a mapping for Coord witha different metric value
-add_factorization((Coord, Factorization, Metric), (Ordered, Matrix), (Ordered1, Matrix1)) :-
-  get_assoc(Coord, Matrix, solution(PrevMetric, _)),
-  !,
-  compound_name_arguments(Metric, MetricName, [N]),
-  compound_name_arguments(PrevMetric, MetricName, [PrevN]),
-  (PrevN < N ->
-    Ordered1 = Ordered,
-    Matrix1 = Matrix
-    ;
-    put_assoc(Coord, Matrix, solution(Metric, [Factorization]), Matrix1),
-    get_assoc(PrevMetric, Ordered, PrevCoordList),
-    subtract(PrevCoordList, [Coord], PrevCoordList1),
-    put_assoc(PrevMetric, Ordered, PrevCoordList1, Ordered_),
-    get_assoc_default(Metric, Ordered_, CoordList, []),
-    put_assoc(Metric, Ordered_, [Coord|CoordList], Ordered1)
-  ).
-% Matrix does not contain any previous mapping for Coord
-add_factorization((Coord, Factorization, Metric), (Ordered, Matrix), (Ordered1, Matrix1)) :-
-  put_assoc(Coord, Matrix, solution(Metric, [Factorization]), Matrix1),
-  get_assoc_default(Metric, Ordered, CoordList, []),
-  put_assoc(Metric, Ordered, [Coord|CoordList], Ordered1).
+explore(((A, B, _), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I_, J_, K), Factorization1, est_size(S1)) :-
+  I_ is I + 1,
+  J_ is J + 1,
+  arg(I_, A, Item),
+  arg(J_, B, Item),
+  Factorization = factorization(LA, LD, _, _, crossed),
+  Factorization1 = factorization([[Item]|LA], [[]|LD], 1, 0, straight),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I_,NB-J_,NC-K], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  S1 is S + 1 + DeltaR.
+explore(((A, B, _), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I_, J_, K), Factorization1, est_size(S1)) :-
+  I_ is I + 1,
+  J_ is J + 1,
+  arg(I_, A, Item),
+  arg(J_, B, Item),
+  Factorization = factorization([FA|LA], LD, SA, SD, straight),
+  SA_ is SA + 1,
+  Factorization1 = factorization([[Item|FA]|LA], LD, SA_, SD, straight),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I_,NB-J_,NC-K], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  (SA < SD -> S1 is S + DeltaR ; S1 is S + 1 + DeltaR).
+explore(((_, _, C), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I, J, K_), Factorization1, est_size(S1)) :-
+  K_ is K + 1,
+  arg(K_, C, Item),
+  Factorization = factorization(LA, LD, _, _, crossed),
+  Factorization1 = factorization([[]|LA], [[Item]|LD], 0, 1, straight),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I,NB-J,NC-K_], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  S1 is S + 1 + DeltaR.
+explore(((_, _, C), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I, J, K_), Factorization1, est_size(S1)) :-
+  K_ is K + 1,
+  arg(K_, C, Item),
+  Factorization = factorization(LA, [FD|LD], SA, SD, straight),
+  SD_ is SD + 1,
+  Factorization1 = factorization(LA, [[Item|FD]|LD], SA, SD_, straight),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I,NB-J,NC-K_], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  (SA > SD -> S1 is S + DeltaR ; S1 is S + 1 + DeltaR).
+explore(((A, _, C), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I_, J, K_), Factorization1, est_size(S1)) :-
+  I_ is I + 1,
+  K_ is K + 1,
+  arg(I_, A, Item),
+  arg(K_, C, Item),
+  Factorization = factorization(LA, LD, _, _, straight),
+  Factorization1 = factorization([[Item]|LA], [[]|LD], 1, 0, crossed),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I_,NB-J,NC-K_], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  S1 is S + 1 + DeltaR.
+explore(((A, _, C), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I_, J, K_), Factorization1, est_size(S1)) :-
+  I_ is I + 1,
+  K_ is K + 1,
+  arg(I_, A, Item),
+  arg(K_, C, Item),
+  Factorization = factorization([FA|LA], LD, SA, SD, crossed),
+  SA_ is SA + 1,
+  Factorization1 = factorization([[Item|FA]|LA], LD, SA_, SD, crossed),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I_,NB-J,NC-K_], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  (SA < SD -> S1 is S + DeltaR ; S1 is S + 1 + DeltaR).
+explore(((_, B, _), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I, J_, K), Factorization1, est_size(S1)) :-
+  J_ is J + 1,
+  arg(J_, B, Item),
+  Factorization = factorization(LA, LD, _, _, straight),
+  Factorization1 = factorization([[]|LA], [[Item]|LD], 0, 1, crossed),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I,NB-J_,NC-K], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  S1 is S + 1 + DeltaR.
+explore(((_, B, _), (NA, NB, NC)),
+        coord(I, J, K), Factorization, est_size(S),
+        coord(I, J_, K), Factorization1, est_size(S1)) :-
+  J_ is J + 1,
+  arg(J_, B, Item),
+  Factorization = factorization(LA, [FD|LD], SA, SD, crossed),
+  SD_ is SD + 1,
+  Factorization1 = factorization(LA, [[Item|FD]|LD], SA, SD_, crossed),
+  max_list([NA-I,NB-J,NC-K], PrevRemain),
+  max_list([NA-I,NB-J_,NC-K], NewRemain),
+  DeltaR = NewRemain - PrevRemain,
+  (SA > SD -> S1 is S + DeltaR ; S1 is S + 1 + DeltaR).
+
+add_factorization((Coord, Factorization, Metric), Register, Register1) :-
+  empty_assoc(Empty),
+  get_assoc_default(Metric, Register, SolutionAssoc, Empty),
+  get_assoc_default(Coord, SolutionAssoc, FactorizationList, []),
+  ord_add_element(FactorizationList, Factorization, FactorizationList1),
+  put_assoc(Coord, SolutionAssoc, FactorizationList1, SolutionAssoc1),
+  put_assoc(Metric, Register, SolutionAssoc1, Register1).
 
 format_factorization(ReverseFactorization, Factorization, Output) :-
   format_factorization(ReverseFactorization, factorization([],[], _), Factorization, [], Output).
